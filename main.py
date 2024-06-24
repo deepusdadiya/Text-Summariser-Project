5c703ba31e6c104609fd2b41969d16cbf1e4d5ed
deepusdadiya <deepusdadiyaofficial@gmail.com>
2024-06-24 11:55:39 +0530

    Main.py changes

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrain
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from src.textSummarizer.logging import logger
-
+import tensorflow as tf
+import torch
+if torch.backends.mps.is_available():
+    torch.mps.empty_cache()
+print("Num GPUs Available (TF): ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("Num GPUs Available (PyTorch): ", torch.cuda.device_count())
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
@@ -14,10 +19,10 @@ except Exception as e:
     logger.exception(e)
     raise e
 
-    
+
 STAGE_NAME = "Data Validation stage"
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<") 
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
@@ -28,7 +33,7 @@ except Exception as e:
 
 STAGE_NAME = "Data Transformation stage"
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<") 
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
@@ -38,7 +43,7 @@ except Exception as e:
 
 
 STAGE_NAME = "Model Trainer stage"
-try: 
+try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
@@ -46,3 +51,12 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+# import tensorflow as tf
+#
+# # Your TensorFlow code here
+# print("TensorFlow version:", tf.__version__)
+#
+# # Check for GPU/Metal support
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
--- src/textSummarizer/components/model_trainer.py
+++ src/textSummarizer/components/model_trainer.py
@@ -14,7 +14,15 @@ class ModelTrainer:
 
     
     def train(self):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        # Check for MPS (Apple Silicon GPU) support
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+
+        print("device:", device)
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
@@ -36,7 +44,7 @@ class ModelTrainer:
             per_device_train_batch_size=1, per_device_eval_batch_size=1,
             weight_decay=0.01, logging_steps=10,
             evaluation_strategy='steps', eval_steps=500, save_steps=1e6,
-            gradient_accumulation_steps=16
+            gradient_accumulation_steps=32
         ) 
 
         trainer = Trainer(model=model_pegasus, args=trainer_args,
